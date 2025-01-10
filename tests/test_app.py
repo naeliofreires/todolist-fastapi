2c3d5a472a8277e4c32f566bea371ed357dd4f3d
Naélio Freires <naeliosistemas@gmail.com>
2025-01-10 10:59:18 -0300

    feat: starting the project

---
+++ tests/test_app.py
@@ -0,0 +1,15 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+
+def test_start():
+
+    client = TestClient(app)
+
+    response = client.get('/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Olá Mundo!'}
